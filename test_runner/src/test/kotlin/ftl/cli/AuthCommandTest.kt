6a7aa60865e69866af15782907480aa6c7a3155a
pawelpasterz <32893017+pawelpasterz@users.noreply.github.com>
2020-11-26 09:18:19 +0100

    Update AuthCommandTest.kt

--- test_runner/src/test/kotlin/ftl/cli/AuthCommandTest.kt
+++ test_runner/src/test/kotlin/ftl/cli/AuthCommandTest.kt
@@ -24,11 +24,5 @@ class AuthCommandTest {
                 "Commands:\n" +
                 "  login"
         )
-        assertThat(output).startsWith(
-            "Manage2 oauth2 credentials for Google Cloud\n\n" +
-            "auth [COMMAND]\n" +
-                "Commands:\n" +
-                "  login"
-        )
     }
 }
