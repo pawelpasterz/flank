bc69810151a03fad5728a1473c1b57451943d884
pawelpasterz <32893017+pawelpasterz@users.noreply.github.com>
2021-01-07 07:05:56 +0100

    Update LastSDKUpdateRun.kt

--- flank-scripts/src/main/kotlin/flank/scripts/shell/firebase/sdk/LastSDKUpdateRun.kt
+++ flank-scripts/src/main/kotlin/flank/scripts/shell/firebase/sdk/LastSDKUpdateRun.kt
@@ -5,5 +5,5 @@ import flank.scripts.github.commons.getLastWorkflowRunDate
 suspend fun getLastSDKUpdateRunDate(token: String) = getLastWorkflowRunDate(
     token = token,
 //     workflowFileName = "update_dependencies_and_client.yml"
-    workflowFileName = "4731769"
+    workflowFileName = "date_pointer.yml"
 )
